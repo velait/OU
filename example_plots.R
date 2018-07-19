d33afdba8edfb95a6db143a1d66c55bff9365f44
velait <villelaitinen@Ville-MacBook-Air.local>
2018-07-19 10:01:37 +0300

    Add example images

--- example_plots.R
+++ example_plots.R
@@ -21,3 +21,11 @@ oup_example_plot2 <- sapply(c(0.01, 0.1, 1), function(x) generate_a_series(sigma
 jpeg("oup_example_plot2.jpg", width = 750, height = 300)
 oup_example_plot2
 dev.off()
+
+
+
+student_oup_example_plot <- sapply(c(3, 10, Inf), function(x) generate_a_series(sigma = 0.05, lambda = 0.1, mu = mu, intervals = 1:200, t.df=x, seed = 1)[["observations"]]) %>% as_tibble() %>% set_colnames(c("df = 3", "df = 10", "df = Inf")) %>%  mutate(time=1:200) %>% melt(id.vars="time") %>% ggplot(aes(x=time, y=value)) + geom_line() + scale_y_continuous(limits = c(2.5, 7.5)) + facet_grid(cols = vars(variable)) + theme_bw() + theme(legend.position="none") + labs(x="Time", y="Value", title="mu = 5, sigma = 0.05")
+
+jpeg("student_oup_example_plot.jpg", width = 750, height = 300)
+student_oup_example_plot
+dev.off()
